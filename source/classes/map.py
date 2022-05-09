064fd3d14472015b9077866f005a9a1c196e7aee
Tharoxes <thc.huynh@protonmail.com>
2022-05-09 14:33:52 +0200

    additional files

--- source/classes/map.py
+++ source/classes/map.py
@@ -1,1 +1,15 @@
 # todo: map class can track and save the routes as .csv or something else
+import numpy as np
+
+
+class Map:
+    def __init__(self):
+        
+
+
+def function(pheronome: np.ndarray):
+    pass
+
+
+def update_pheromones():
+    pass
---
+++ source/formulas.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+import heapq
+import math
+
+
+def generate_ants():
+    pass
+
+def calculate_pheromones():
+    pass
