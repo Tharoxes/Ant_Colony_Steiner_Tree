3c6f52307f69f3390cd677b2a9f2f2f20bda174c
Lucien Lang <93982398+wuridump@users.noreply.github.com>
2022-05-09 14:52:22 +0200

    Update ant.py
    Ant basic structure

--- source/classes/ant.py
+++ source/classes/ant.py
@@ -7,4 +7,32 @@ ant class
 """
 
 class Ant:
-    def __init__(self)
+    def __init__(self, position):
+        """position = index of current node, nodes = must visit nodes, path = travelled path, visited = already visited nodes (must visit nodes)
+        visited_all : a bolean to check if a ant has visited all must visit nodes"""
+        self.nodes = []
+        self.path = []
+        self.position = position
+        self.visited = []
+        self.visited_all = False
+    
+    def update_position(self, pos):
+        self.position = pos
+        if(pos in self.nodes and not pos in self.visited):
+            self.visited.append(pos)
+            
+        if len(self.visited) == len(self.nodes):
+            self.visited_all = True
+        
+        
+    def update_path(self, edge: tuple):
+        if(edge in self.path): return
+        if((edge[1], edge[0]) in self.path): return 
+        
+        self.path.append(edge)
+        
+
+        
+    
+        
+        
