4c26ea37c3347511b3278129ad48c809f10b78b8
Lucien Lang <93982398+wuridump@users.noreply.github.com>
2022-05-09 15:12:48 +0200

    Update ant.py
    Ant classs with working position, visited nodes, and update of the path

--- source/classes/ant.py
+++ source/classes/ant.py
@@ -7,32 +7,37 @@ ant class
 """
 
 class Ant:
-    def __init__(self, position):
+    def __init__(self, position: int):
         """position = index of current node, nodes = must visit nodes, path = travelled path, visited = already visited nodes (must visit nodes)
         visited_all : a bolean to check if a ant has visited all must visit nodes"""
         self.nodes = []
         self.path = []
         self.position = position
-        self.visited = []
+        self.visited = [position]
         self.visited_all = False
     
-    def update_position(self, pos):
-        self.position = pos
-        if(pos in self.nodes and not pos in self.visited):
-            self.visited.append(pos)
-            
+    def update_position(self, newPos: int):
+        """updates the position of the ant"""
+        #get the travelled edge
+        travelled_edge = (self.position, newPos)
+        
+        #check if the newPos is in the must visit nodes and not already visited
+        if(newPos in self.nodes and not newPos in self.visited):
+            self.visited.append(newPos)
+        
+        #checks if all must visit nodes were visited
         if len(self.visited) == len(self.nodes):
             self.visited_all = True
+         
+        #updates the Ant
+        self.update_path(travelled_edge)
+        self.position = newPos
+        
         
         
     def update_path(self, edge: tuple):
+        """helper function for updating a travelled edge"""
         if(edge in self.path): return
         if((edge[1], edge[0]) in self.path): return 
         
         self.path.append(edge)
-        
-
-        
-    
-        
-        
