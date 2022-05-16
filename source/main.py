18331afc9fa96fc404f5e7c088a8d90b786830f1
Lucien Lang <93982398+wuridump@users.noreply.github.com>
2022-05-16 14:52:03 +0200

    moved move_ant to main
    moved move_ant to main

--- source/main.py
+++ source/main.py
@@ -18,6 +18,33 @@ import math
 from classes import ant
 
 
+def move_ant(ant: Ant):
+    """moves an Ant to a new node"""
+    """rewrite to ask map an index -> map returns the possible next ones and their prob"""
+    # possible = nodes.connect(ant.position) #i somehow need the possible conections from a given node
+    
+    # smart_choice_prob = 0.75 #probability to make the optimal choice
+    # #make not smart choice
+    # if(not (rd.random() < smart_choice_prob)):
+    #     ant.update_position(np.choice(possible, 1))
+    
+    # #find the best next choice -> assumption pheromones are saved as a tuple (i,j)
+    # best_next = (possible[0], pheromones[ant.pos, possible[0]])
+    # for i in range(1, len(possible)):
+    #     pheromones_onedge = pheromones[ant.pos, possible[i]]
+    #     if  pheromones_onedge > best_next[1]:
+    #         best_next = (possible[i], pheromones_onedge)
+            
+    # ant.update_position(best_next[0])
+    
+    possible = Map.get_possible(ant.pos)
+    possible = np.transpose(possible)
+    
+    move_to = np.random.choice(possible[0], p = possible[1])#index index list, prob = probabilities
+    
+    ant.update(move_to)
+    return
+
 def main():
     # generate ants in a list
 
