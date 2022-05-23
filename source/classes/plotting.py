8a22401d9408485d720e84e839c24add2bef109a
Lucien Lang <93982398+wuridump@users.noreply.github.com>
2022-05-23 16:20:10 +0200

    plotting working
    needs maps in a list an makes an animation from that list

--- source/classes/plotting.py
+++ source/classes/plotting.py
@@ -13,12 +13,17 @@
 from bokeh.io import output_file, show, save, curdoc
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource
+from bokeh.layouts import layout
 import map as mp
 import numpy as np
+from bokeh.transform import linear_cmap
+from bokeh.palettes import RdYlBu, Turbo256
+import time
 
-def plot_one(map: mp.Map):
-    """plots the map
-    Remark: Because of normalizatio not all edges are always visible"""
+def plot_init(map: mp.Map):
+    """plots the map"""
+    palette = Turbo256  #if an other palette is used change this
+    
     nodes = map.node_list
     nodes_coor = []
     colorlist = []
@@ -56,39 +61,130 @@ def plot_one(map: mp.Map):
         })
     
     
-                 
+    edge_color = linear_cmap(field_name = "pher", palette = palette, low= 0,  high = 1)     
     
     
     
     
-    p = figure()
+    p = figure(width = 940, height = 940)
     p.circle(x = "x", y = "y", color = "color", source = node_source)
-    p.segment(x0 = "x0", y0 = "y0", x1 = "x1", y1 = "y1", color = "black", alpha = "pher", line_width = 1, source = edge_source)
+    p.segment(x0 = "x0", y0 = "y0", x1 = "x1", y1 = "y1", color = edge_color, alpha = 1, line_width = 1, source = edge_source)
     
-    show(p)
+    # show(p)
     
     
-    return 
+    return p, edge_source, node_source
+
+def update_sources():
+    global maplist
+    global mapindex
+    global node_source
+    global edge_source
+    mapindex += 1
+    mapindex %= len(maplist)
+    
+    print(mapindex)
+    map = maplist[mapindex]
+    nodes = map.node_list
+    nodes_coor = []
+    colorlist = []
+    for key in nodes.keys():
+        color = "green"
+        if nodes[key]["real"]:
+            color = "firebrick"
+            
+        colorlist.append(color)
+        nodes_coor.append(nodes[key]["position"])
+    
+    
+    nodes_coor = np.array(nodes_coor)
+    node_source.data = {
+        "x": nodes_coor[:,0], 
+        "y": nodes_coor[:,1],
+        "color": colorlist}
+    
+    paths = map.paths
+    x0, x1, y0, y1 = [],[],[],[]
+    for edge in paths:
+        x0.append(nodes[edge[0]]["position"][0])
+        y0.append(nodes[edge[0]]["position"][1])
+        x1.append(nodes[edge[1]]["position"][0])
+        y1.append(nodes[edge[1]]["position"][1])
+     
+           
+    
+    edge_source.data = {
+        "x0": x0,
+        "y0": y0,
+        "x1": x1,
+        "y1": y1,
+        "pher": (np.array(map.pheromone) - np.min(map.pheromone))/ (np.max(map.pheromone)- np.min(map.pheromone)) 
+        }
+    
+    print(edge_source.data)
+    
+    return
 
 
 
-def main():
-
-    node_dict = {
-        1: {"position": np.asarray([0,1]), "real": True},
-        2: {"position": np.asarray([0,0]), "real": True},
-        3: {"position": np.asarray([2,1]), "real": True},
-        4: {"position": np.asarray([2,0]), "real": True},
-    }
-    
-    paths = [[1,2], [2,3], [1,3], [2,4]]
-    
-    pheromones = [60, 50, 50, 50]
-    
-    mapi = mp.Map(node_list=node_dict, paths=paths, pheromones=pheromones)
-    
-    plot_one(mapi)
+# def plot_all():
 
 
-if __name__ == "__main__":
-    main()
+
+
+node_dict = {
+    1: {"position": np.asarray([0,1]), "real": True},
+    2: {"position": np.asarray([0,0]), "real": True},
+    3: {"position": np.asarray([2,1]), "real": True},
+    4: {"position": np.asarray([2,0]), "real": True},
+}
+
+paths = [[1,2], [2,3], [1,3], [2,4]]
+
+pheromones = [100, 50, 80, 10]
+
+mapi = mp.Map(node_list=node_dict, paths=paths, pheromones=pheromones)
+
+
+# plot, edge_source, node_source = plot_init(mapi)
+maplist = [mapi]
+
+
+# show(plot)
+# while True:
+for k in range(0,10):
+    # print(k)
+    path = []
+    pheromones = []
+    for idx, edge in enumerate(paths):
+        path.append([np.random.randint(1,5), np.random.randint(1,5)])
+        pheromones.append(np.random.randint(0,100))
+        
+    # print(paths)
+    mapi = mp.Map(node_list=node_dict, paths=path, pheromones=pheromones)
+    maplist.append(mapi)
+
+paths = [[1,4], [2,3], [1,2], [2,4]]
+pheromones = [90, 20, 3, 10]
+
+mapi = mp.Map(node_list=node_dict, paths=paths, pheromones=pheromones)
+
+
+
+"""everithyng below here needs to be in the main file"""
+maplist.append(mapi)
+        
+      
+mapindex = 0
+plot, edge_source, node_source = plot_init(maplist[mapindex])
+
+doc = curdoc()
+doc.add_root(plot)
+doc.add_periodic_callback(update_sources, 1000)
+
+
+
+    
+    
+
+
